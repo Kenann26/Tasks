0fb7cc61b044be21bee7483a45e165c2e4c61722
Kenann26 <kenann.nebiyev@gmail.com>
2024-05-24 05:19:54 +0400

    Task

---
+++ Task/Task/Program.cs
@@ -0,0 +1,28 @@
+﻿        int number = 12;
+        
+        if (number<=1)
+        {
+         Console.WriteLine($"{number}:Eded ne sade ne de murekkeb ededdir" );
+         }
+        else 
+        {
+            int Count = 0;
+        for (int i = 1; i <= number; i++)
+        {
+            if (number % i == 0)
+            {
+                Count++;
+            }
+        }
+
+        if (Count == 2)
+        {
+            Console.WriteLine("Eded sade ededdir");
+        }
+        else
+        {
+            Console.WriteLine("Eded murekkeb ededdir");
+        }  
+        }
+    
+
